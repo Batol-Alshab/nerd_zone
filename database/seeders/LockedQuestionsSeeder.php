92942455a786bcf894aacf4c779d05443e8f32a5
Batol_Alshab <batolsh2003@gmail.com>
2024-04-02 16:33:13 +0300

    new locked question

---
+++ database/seeders/LockedQuestionsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Locked_question;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class LockedQuestionsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Locked_question::create([
+            'url'=>'الاختبار الاول.pdf',
+            'title' => 'الاختبار الاول',
+            'rate'=>10,
+            'unit_id'=> '1',
+        ]);   
+        // DB::table('locked_questions')->insert([
+        //     'url' => 'example.com/locked-question-1',
+        //     'title' => 'Locked Question 1',
+        //     'rate' => 5,
+        //     'unit_id' => 1, // يجب أن يكون هذا القيمة موجودة في جدول 'units'
+        // ]);
+    }
+}
